c0f06a25a0ff905c8c0609f4a6d72ca49913019c
rejuanigg <juanisnm1995@gmail.com>
2024-04-08 07:30:50 -0300

    errores gramaticales solucionados

--- calculator.py
+++ calculator.py
@@ -1,115 +1,48 @@
 # CALCULADORA
+# Usar consola de python, introducí el numero X luego la operacion y por ultimo un numero Y
 
-from tkinter import *
+a = int(input("Dame a "))
+o = str(input("introducí operación "))
+if o == "#":
+    b  = 0
+else: b = int(input("Dame b "))
 
-ventana = Tk ()
-ventana.title("Calculadora")
+def suma (a, b):
+    resultado = a + b 
+    return resultado
 
-#VENTANA
+def multiplier (a, b):
+    resultado = a * b 
+    return resultado
 
-input_text = Entry(ventana, font=("Calibri 20"))
-input_text.grid(row = 0, column= 0, columnspan=4, padx= 10, pady=5)
+def divisor (a, b):
+    resultado = a / b 
+    return resultado
 
-#BOTONES
-boton1 = Button(ventana, text = "1", width= 5, height= 2)
-boton2 = Button(ventana, text = "2", width= 5, height= 2)
-boton3 = Button(ventana, text = "3", width= 5, height= 2)
-boton4 = Button(ventana, text = "4", width= 5, height= 2)
-boton5 = Button(ventana, text = "5", width= 5, height= 2)
-boton6 = Button(ventana, text = "6", width= 5, height= 2)
-boton7 = Button(ventana, text = "7", width= 5, height= 2)
-boton8 = Button(ventana, text = "8", width= 5, height= 2)
-boton9 = Button(ventana, text = "9", width= 5, height= 2)
-boton0 = Button(ventana, text = "0", width= 13, height= 2)
-
-boton_ac = Button(ventana, text = "AC", width= 5, height= 2)
-boton_p1 = Button(ventana, text = "(", width= 5, height= 2)
-boton_p2 = Button(ventana, text = ")", width= 5, height= 2)
-boton_point = Button(ventana, text = ".", width= 5, height= 2)
-
-boton_division = Button(ventana, text = "/", width= 5, height= 2)
-boton_multiplicacion = Button(ventana, text = "*", width= 5, height= 2)
-boton_suma = Button(ventana, text = "+", width= 5, height= 2)
-boton_resta = Button(ventana, text = "-", width= 5, height= 2)
-boton_igual = Button(ventana, text = "=", width= 5, height= 2)
-
-#AGREGAR BOTONES A LA PANTALLA
-
-
-
-boton_ac.grid(row= 1, column= 0, padx= 5, pady= 5)
-boton_p1.grid(row= 1, column= 1, padx= 5, pady= 5)
-boton_p2.grid(row= 1, column= 2, padx= 5, pady= 5)
-
-boton_division.grid(row= 1, column= 3, padx= 5, pady= 5)
-
-boton7.grid(row=2 , column=0 , padx=5 , pady=5)
-boton8.grid(row=2 , column=1 , padx=5 , pady=5)
-boton9.grid(row=2 , column=2 , padx=5 , pady= 5)
-
-boton_multiplicacion.grid(row= 2, column=3 , padx= 5, pady= 5)
-
-boton4.grid(row= 3, column= 0, padx= 5, pady= 5)
-boton5.grid(row= 3, column= 1, padx= 5, pady= 5)
-boton6.grid(row= 3, column= 2, padx= 5, pady= 5)
-
-boton_suma.grid(row= 3, column= 3, padx= 5, pady= 5)
-
-boton1.grid(row= 4, column= 0, padx= 5, pady= 5)
-boton2.grid(row= 4, column= 1, padx= 5, pady= 5)
-boton3.grid(row= 4, column= 2, padx= 5, pady= 5)
-
-boton_resta.grid(row= 4, column= 3, padx= 5, pady= 5)
-
-boton0.grid(row= 5, columnspan= 2, column=0 , padx= 10, pady= 5)
-boton_point.grid(row= 5, column= 2, padx= 5, pady= 5)
-boton_igual.grid(row= 5, column= 3, padx= 5, pady= 5)
-
-ventana.mainloop()
-
-
-# a = int(input("Dame a "))
-# o = str(input("introducí operación "))
-# if o == "#": 
-#     b = 0
-# else: b = int(input("Dame b "))
-
-# def suma (a, b):
-#     resultado = a + b 
-#     return resultado
-
-# def multiplier (a, b):
-#     resultado = a * b 
-#     return resultado
-
-# def divisor (a, b):
-#     resultado = a / b 
-#     return resultado
-
-# def dif (a, b):
-#     resultado = a - b 
-#     return resultado
+def dif (a, b):
+    resultado = a - b 
+    return resultado
     
-# def potencia (a, b):
-#     resultado = a ** b
-#     return resultado
+def potencia (a, b):
+    resultado = a ** b
+    return resultado
 
-# def raiz (a):
-#     resultado = a ** 0.5
-#     return resultado 
+def raiz (a):
+    resultado = a ** 0.5
+    return resultado 
 
-# def operacion (o):
-#     return
-# if  o == "+":
-#     print(suma(a, b))
-# elif o == "^":
-#     print(potencia(a, b))
-# elif o == "#":
-#     print (raiz(a))
-# elif o == "/":
-#     print ( int (divisor(a,b)))
-# elif o == "*" :
-#     print(multiplier(a,b))
-# elif o == "-":
-#     print(dif(a,b))
-# else: print("no introdujo una operación válida")
+def operacion (o):
+    return
+if  o == "+":
+    print(suma(a, b))
+elif o == "^":
+    print(potencia(a, b))
+elif o == "#":
+    print (raiz(a))
+elif o == "/":
+    print ( int (divisor(a,b)))
+elif o == "*" :
+    print(multiplier(a,b))
+elif o == "-":
+    print(dif(a,b))
+else: print("No introdujo una operación válida")
